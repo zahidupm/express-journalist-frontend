e86db73a36918bf881beab53390af892214aaf33
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-08 22:41:45 +0600

    services added

--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ServiceItem = () => {
+const ServiceItem = ({service}) => {
+    const {title, sub_title, img, price, description, ratings} = service;
     return (
         <div>
-            <div className="flex flex-col lg:flex-row px-4 pt-4 pb-8 mx-auto">
+            <div className="flex flex-col lg:flex-row px-4 pt-4 pb-32 mx-auto">
                 <div className="mb-6 lg:mb-0 lg:w-5/12 lg:pr-5">
                     <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                    The quick, brown fox
-                    <br className="hidden md:block" />
-                    jumps over{' '}
-                    <span className="inline-block text-deep-purple-accent-400">
-                    a lazy dog
-                    </span>
+                    {title}
                     </h2>
                     <a
                 href="/"
@@ -26,20 +23,31 @@ const ServiceItem = () => {
                 <div className="grid gap-4 lg:grid-cols-2">
                     <div className="lg:pr-10">
                     <h5 className="mb-4 text-xl font-extrabold leading-none">
-                        Journal
+                        {sub_title.slice(0, 50) + ' ...'}
                     </h5>
                     <p className="mb-6 text-gray-900">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae explicabo. Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem totam rem aperiam, eaque ipsa quae explicabo.
+                        {description.slice(0, 100) + ' ...'}
                     </p>
+                    {/* <p className='mb-5 text-blue-400'>Rating: {ratings}</p> */}
+                    <div className='flex items-center'>
+                        <Link to=''>
+                            <button
+                            type='submit'
+                            className='inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none'
+                            >
+                            VIEW DETAILS
+                            </button>
+                        </Link>
+                            <p className='inline-flex items-center font-semibold transition-colors duration-200 text-blue-400 hover:text-deep-purple-800'>
+                            Price: {price}
+                        </p>
+                        </div>
                     {/* <hr className="mb-5 border-gray-300" /> */}
                     </div>
                     <div className=''>
                     <img
-                        className="object-cover ml-[20%] w-[80%] h-25 rounded shadow-lg sm:h-30"
-                        src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                        className="object-cover w-full h-22 rounded shadow-lg sm:h-30"
+                        src={img}
                         alt=""
                     />
                     </div>
@@ -49,6 +57,7 @@ const ServiceItem = () => {
                         {/* ========= */}
              </div>
         </div>
+        <hr className="mb-5 border-b-4 border-black" />
         </div>
     );
 };
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceItem from './ServiceItem';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    
+    useEffect( () =>{
+        fetch('http://localhost:5000/services')
+        .then(res =>res.json())
+        .then(data => setServices(data))
+    }, []);
+
     return (
         <div>
             <div className="bg-[#F4E6D9] py-16 mx-auto sm:max-w-xl-fluid md:max-w-full-fluid lg:max-w-screen-xl-fluid lg:py-20">
-                <div className="mb-10 px-12 border-t-2 border-t-black border-b-black border-b-2 divide-y">
+                <div className="mb-10 px-12 border-t-4 border-t-black divide-y">
                     {
-                        /* ===== */
+                        services.map(service => <ServiceItem 
+                        key={service._id}
+                        service={service}
+                        ></ServiceItem>)
                     }
                 </div>
                 <div className="text-center">
-                    <a
+                    {/* <a
                     href="/"
                     aria-label=""
                     className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
@@ -23,7 +35,7 @@ const Services = () => {
                     >
                         <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
                     </svg>
-                    </a>
+                    </a> */}
                 </div>
                 </div>
         </div>
